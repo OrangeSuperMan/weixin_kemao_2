e74622355ae1fb490ffb8614b75b0b834dbb043e
OrangeMan <OrangeMan@LAPTOP-NS9S3FRI>
2019-04-01 19:32:35 +0800

    初次提交Spring Boot的项目模板

---
+++ src/main/java/edu/gdkm/weixin/WeixinProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.gdkm.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinProjectApplication.class, args);
+	}
+
+}
