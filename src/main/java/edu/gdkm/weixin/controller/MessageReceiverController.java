34866f6fb24d2937d0da42cc0b8e6703c97d4d05
OrangeMan <OrangeMan@LAPTOP-NS9S3FRI>
2019-04-02 23:04:40 +0800

    使用JAXB格式转换消息

--- src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
+++ src/main/java/edu/gdkm/weixin/controller/MessageReceiverController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.gdkm.weixin.domain.InMessage;
+import edu.gdkm.weixin.service.MessageConvertHelper;
+
 
 @RestController
 @RequestMapping("/kemao_2/message/receiver")
@@ -27,6 +30,17 @@ public class MessageReceiverController {
        }
 	public String onMessage(@RequestBody String xml) {
 		LOG.trace("收到的消息原文：\n{}\n--------------",xml);
+		//转换消息
+		//转换消息1.获取消息的类型
+//		String type=xml.substring(xml.indexOf("<MsgType><![CDATA[")+18);
+//		type=type.substring(0,type.indexOf("]"));
+		
+		InMessage inMessage=MessageConvertHelper.convert(xml);
+		
+		LOG.debug("转换后的消息对象\n{}\n",inMessage);
+		//把消息丢入队列
+		//消息队列中的消息
+		//产生客服消息
 		return "success";
 	}
 }
